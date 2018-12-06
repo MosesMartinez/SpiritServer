08db9c168f40ad8037918474dcd763fc79084b52
Dylan Wright <sealsurlaw@gmail.com>
2018-12-06 00:20:14 -0800

    Hashed password login

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var hash = require('password-hash');
 var createError = require('http-errors');
 
 var db = require('../db');
 
 router.post('/', function(req, res, next) {
     var email = req.body.email;
-    var password = req.body.password;
 
-    db.any(`SELECT * FROM users WHERE user_email='`+email+`' AND user_password='`+password+`'`)
+    db.any(`SELECT * FROM users WHERE user_email='`+email+`'`)
     .then( user => {
-        if (user.length > 0)
+        if (user.length > 0 && hash.verify(req.body.password, user[0].user_password))
             res.json(user[0].user_token);
         else {
             next(createError(401));
