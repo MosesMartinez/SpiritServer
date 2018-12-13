ac02d0c0d55b515fe66282f309d15e0a4144231a
Dylan Wright <sealsurlaw@gmail.com>
2018-12-13 14:51:35 -0800

    Format date

--- routes/read.js
+++ routes/read.js
@@ -53,9 +53,9 @@ router.get('/:token', function(req, res, next) {
                            alcohol: new Array()});
 
                 for (let j = 0; j < machines[i].machine_alcohol.length; ++j) {
-                    var seconds = new Date(machines[i].machine_empty_time);
-                    machines[i].machine_empty_time = seconds.getSeconds();
-                    console.log(seconds);
+                    // var seconds = new Date(machines[i].machine_empty_time);
+                    // machines[i].machine_empty_time = seconds.getSeconds();
+                    console.log(machines[i].machine_empty_time);
                     let alc = {type: machines[i].machine_alcohol[j],
                                full: machines[i].machine_full[j],
                                container: machines[i].machine_container[j],
