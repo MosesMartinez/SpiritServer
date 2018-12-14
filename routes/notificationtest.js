82a9e3f10e83facece0e76b1cadd65578ad1aa05
Dylan Wright <sealsurlaw@gmail.com>
2018-12-13 19:05:15 -0800

    Added notification

--- routes/notificationtest.js
+++ routes/notificationtest.js
@@ -8,39 +8,39 @@ var db = require('../db');
 router.get('/:token', function(req, res, next) {
     var deviceToken = req.params.token;
 
-    db.any(`SELECT * FROM users`)
-    .then( user => {
-        res.send(user);
-    })
+    // db.any(`SELECT * FROM users`)
+    // .then( user => {
+    //     res.send(user);
+    // })
 
 
-    // var options = {
-    //     token: {
-    //         key: "keys/AuthKey_494J327684.p8",
-    //         keyId: "494J327684",
-    //         teamId: "2X829ERV72"
-    //     },
-    //     production: false
-    // };
+    var options = {
+        token: {
+            key: "keys/AuthKey_494J327684.p8",
+            keyId: "494J327684",
+            teamId: "2X829ERV72"
+        },
+        production: false
+    };
     
-    // var apnProvider = new apn.Provider(options);
+    var apnProvider = new apn.Provider(options);
 
-    // // let deviceToken = "95333529ED1F2B665C9A42A227057089D575A5643E1D6D71F4038E576D25144D"
+    let deviceToken = "90A6B007CF19B00A63A8820B2582632240B1341B9178F53870C4184BB680E2D2"
 
-    // var note = new apn.Notification();
+    var note = new apn.Notification();
     
-    // note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-    // note.badge = 3;
-    // note.body = "Vodka is low";
-    // note.title = "Spirit Machine"
-    // note.payload = {'messageFrom': 'John Appleseed'};
-    // note.topic = "com.spiritmachine.SpiritMachineTechnician";
+    note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+    note.badge = 3;
+    note.body = "Vodka is low";
+    note.title = "Spirit Machine"
+    note.payload = {'messageFrom': 'John Appleseed'};
+    note.topic = "com.spiritmachine.SpiritMachineTechnician";
 
-    // res.json(note);
+    res.json(note);
 
-    // apnProvider.send(note, deviceToken).then( (result) => {
-    //     console.log(result);
-    // });
+    apnProvider.send(note, deviceToken).then( (result) => {
+        console.log(result);
+    });
 });
 
 module.exports = router;
