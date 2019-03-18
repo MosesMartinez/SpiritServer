1404580bf951f83f0fe1f95dd1e3104212a2b4ca
Dylan Wright <sealsurlaw@gmail.com>
2019-03-18 03:09:57 -0700

    Checking if name exists before adding cocktail

--- routes/cocktails.js
+++ routes/cocktails.js
@@ -93,7 +93,7 @@ router.get('/:machine/', function (req, res, next) {
                                 },
                             };
 
-                            if (!cocObj.cocktails.includes(curCocktail.name))
+                            if (!cocObj.cocktails.includes(curCocktail))
                                 cocObj.cocktails.push(curCocktail);
                         }
                     });
