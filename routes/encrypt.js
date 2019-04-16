838efb9eaf402f005b7a562b17b73fba37852ffe
Dylan Wright <sealsurlaw@gmail.com>
2019-04-15 20:07:20 -0700

    Fixed keys

--- routes/encrypt.js
+++ routes/encrypt.js
@@ -1,15 +1,13 @@
-var express = require('express');
-var router = express.Router();
-var db = require('../db');
-var aesjs = require('aes-js');
-var hex64 = require('hex64');
+let express = require('express');
+let router = express.Router();
+let db = require('../db');
+let aesjs = require('aes-js');
+let hex64 = require('hex64');
+let key = require('../keys/encryptKey').key;
 
 router.post('/:nfc', function (req, res, next) {
-    var nfcID = req.params.nfc;
-
-    // AES128 Encryption
-    // An example 128-bit key (16 bytes * 8 bits/byte = 128 bits)
-    var key = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    let nfcID = req.params.nfc;
+    console.log(key);
 
     db.any(`SELECT user_email FROM users WHERE user_id=` + nfcID + `;`)
         .then(email => {
@@ -25,34 +23,34 @@ router.post('/:nfc', function (req, res, next) {
             console.log("JSON String: " + jsonString);
 
             // Convert text to bytes
-            var jsonBytes = aesjs.utils.utf8.toBytes(jsonString);
+            let jsonBytes = aesjs.utils.utf8.toBytes(jsonString);
 
             // The counter is optional, and if omitted will begin at 1
-            var aesCtr = new aesjs.ModeOfOperation.ctr(key);
-            var encryptedBytes = aesCtr.encrypt(jsonBytes);
+            let aesCtr = new aesjs.ModeOfOperation.ctr(key);
+            let encryptedBytes = aesCtr.encrypt(jsonBytes);
             console.log('JSON bytes: ' + jsonBytes);
 
             // To print or store the binary data, you may convert it to hex
-            var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+            let encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
             console.log('Hex: ' + encryptedHex);
-            var encryptedBase64 = hex64.encode(encryptedHex);
+            let encryptedBase64 = hex64.encode(encryptedHex);
             console.log('Base64: ' + encryptedBase64);
 
-            var newEncryptedHex = hex64.decode(encryptedBase64);
+            let newEncryptedHex = hex64.decode(encryptedBase64);
             console.log('New Hex: ' + newEncryptedHex);
 
-            var encryptedBytes = aesjs.utils.hex.toBytes(newEncryptedHex);
+            let encryptedBytes = aesjs.utils.hex.toBytes(newEncryptedHex);
 
             // The counter mode of operation maintains internal state, so to
             // decrypt a new instance must be instantiated.
-            var aesCtr = new aesjs.ModeOfOperation.ctr(key);
-            var decryptedBytes = aesCtr.decrypt(encryptedBytes);
+            let aesCtr = new aesjs.ModeOfOperation.ctr(key);
+            let decryptedBytes = aesCtr.decrypt(encryptedBytes);
 
             // Convert our bytes back into text
-            var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+            let decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
             console.log(decryptedText);
 
-            //var decryptedJSON = JSON.parse(decryptedText);
+            //let decryptedJSON = JSON.parse(decryptedText);
             console.log('Decrypted is: ' + decryptedText);
 
             res.send(encryptedBase64);
