191999731d480bfd5df6e5a832616f770e74cb54
Dylan Wright <sealsurlaw@gmail.com>
2019-04-23 01:54:06 -0700

    Updated wallet to include only tokens

--- server/wallet.js
+++ server/wallet.js
@@ -1,46 +1,59 @@
-let app = require('express')();
-let db = require('../db');
-let aesjs = require('aes-js');
-let hex64 = require('hex64');
+const app = require('express')();
+const db = require('../db');
+// const aesjs = require('aes-js');
+// const hex64 = require('hex64');
+const hash = require('object-hash');
 
-app.post('/:nfc', function (req, res, next) {
-    let nfcData = req.params.nfc;
-    let key = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+// Create new wallet
+app.post('/', (req, res) => {
+    const token = hash(Date.now());
 
-    console.log('Encrypted: ' + nfcData);
+    db.any(`INSERT INTO wallets(
+        token, money)
+        VALUES ('${token}', ${0.00}) RETURNING token;
+        `)
+        .then(token => {
+            console.log(token[0]);
+            res.send(token[0]);
+        })
+        .catch(err => {
+            console.log(err);
+            res.send(err);
+        })
+})
 
-    // Convert Base64 to Hex
-    //let encryptedHex = hex64.decode(nfcData);
+// Update balance
+app.post('/:token/:newBalance', (req, res) => {
+    const token = req.params.token;
+    const newBalance = req.params.newBalance;
 
-    // Decrypt encrypted NFC data
-    let encryptedBytes = aesjs.utils.hex.toBytes(nfcData);
-    console.log("Became bytes");
-    console.log("Bytes:\b" + encryptedBytes);
-
-    // The counter mode of operation maintains internal state, so to
-    // decrypt a new instance must be instantiated.
-    let aesCtr = new aesjs.ModeOfOperation.ctr(key);
-    console.log("new aesCtr made");
-    let decryptedBytes = aesCtr.decrypt(encryptedBytes);
-    console.log("Decrypted bytes");
-
-    // Convert our bytes back into text
-    let decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
-    console.log("Decrypted text; " + decryptedText);
-
-    let decryptedJSON = JSON.parse(decryptedText);
-    let id = decryptedJSON.id;
-    let email = decryptedJSON.email;
-
-    db.any(`SELECT user_money FROM users WHERE user_id=` + id + ` AND user_email='` + email + `';`)
+    db.any(`UPDATE wallets
+	        SET money=${newBalance}
+            WHERE token='${token}'
+            RETURNING money;`)
         .then(money => {
-            console.log(money);
-            res.json({
-                id: id,
-                money: money[0].user_money
-            })
-        });
+            console.log(money[0]);
+            res.send(money[0]);
+        })
+        .catch(err => {
+            console.log(err);
+            res.send("User not found");
+        })
+});
 
+// Get money value
+app.get('/:token', function (req, res, next) {
+    const token = req.params.token;
+
+    db.any(`SELECT money FROM wallets WHERE token='${token}';`)
+        .then(money => {
+            console.log(money[0]);
+            res.send(money[0]);
+        })
+        .catch(err => {
+            console.log(err);
+            res.send("User not found");
+        })
 
 });
 
