96dc9caff81818bd660a518a096b10c6c4eb0b63
Dylan Wright <sealsurlaw@gmail.com>
2019-04-23 03:33:22 -0700

    Allowed storage of cocktail json objects

--- server/cocktails.js
+++ server/cocktails.js
@@ -1,6 +1,13 @@
-var app = require('express')();
-var cocktails = require('../js/liquors').liquors;
-var db = require('../db');
+let app = require('express')();
+let bodyParser = require('body-parser');
+let cocktails = require('../js/liquors').liquors;
+let db = require('../db');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/', function (req, res, next) {
     let alcohols = req.query.alcohols.split(',');
@@ -35,96 +42,130 @@ app.get('/', function (req, res, next) {
     res.send(cocktailArray);
 });
 
-app.get('/:machine/', function (req, res, next) {
+// app.get('/:machine/', function (req, res, next) {
+//     let machine = req.params.machine;
+
+//     // Make sure no SQL injection
+//     if (!Number.isInteger(parseInt(machine))) {
+//         res.send("Not a valid input");
+//         return;
+//     }
+
+//     db.any(`SELECT machine_alcohol, machine_mixer FROM machines WHERE machine_id = ` + machine + `;`)
+//         .then(result => {
+//             alcohols = result[0].machine_alcohol;
+//             mixers = result[0].machine_mixer;
+
+//             console.log("Alcohol: " + alcohols);
+//             console.log("Mixer: " + mixers);
+
+//             let cocktailObj = []
+
+//             // [
+//             //     {
+//             //         alcohol: null,
+//             //         cocktails: [
+//             //             {
+//             //                 name: null,
+//             //                 price: null,
+//             //                 image: null,
+//             //             },
+//             //         ]
+//             //     }
+//             // ]
+
+//             alcohols.forEach(alc => {
+//                 cocktailObj.push(
+//                     {
+//                         alcohol: alc,
+//                         cocktails: [],
+//                     }
+//                 );
+//             });
+
+//             cocktailObj.forEach((cocObj, alcNum) => {
+//                 mixers.forEach((mix, mixNum) => {
+//                     cocktails.forEach(coc => {
+//                         if (coc.alcohols.includes(cocObj.alcohol) && coc.mixers.includes(mix)) {
+//                             const curCocktail =
+//                             {
+//                                 name: coc.name,
+//                                 price: 5.00,
+//                                 image: coc.image,
+//                                 alcohol: {
+//                                     name: cocObj.alcohol,
+//                                     container: alcNum,
+//                                 },
+//                                 mixer: {
+//                                     name: mix,
+//                                     container: mixNum,
+//                                 },
+//                             };
+
+//                             if (!cocObj.cocktails.includes(curCocktail))
+//                                 cocObj.cocktails.push(curCocktail);
+//                         }
+//                     });
+//                 });
+
+//                 // Sort by name
+//                 cocObj.cocktails.sort((a, b) => {
+//                     return a.name.localeCompare(b.name);
+//                 });
+
+//                 // Remove duplicates
+//                 for (let i = 0; cocObj.cocktails[i] != null; ++i) {
+//                     if (cocObj.cocktails[i + 1] != null &&
+//                         cocObj.cocktails[i].name == cocObj.cocktails[i + 1].name) {
+//                         cocObj.cocktails.splice(i + 1, 1);
+//                         --i;
+//                     }
+//                 }
+//             });
+
+//             console.log(cocktailObj);
+
+//             res.send(cocktailObj);
+
+//         })
+//         .catch(err => {
+//             console.log(err);
+//         });
+
+// });
+
+app.get('/:machine', (req, res) => {
     let machine = req.params.machine;
 
-    // Make sure no SQL injection
-    if (!Number.isInteger(parseInt(machine))) {
-        res.send("Not a valid input");
-        return;
-    }
-
-    db.any(`SELECT machine_alcohol, machine_mixer FROM machines WHERE machine_id = ` + machine + `;`)
-        .then(result => {
-            alcohols = result[0].machine_alcohol;
-            mixers = result[0].machine_mixer;
-
-            console.log("Alcohol: " + alcohols);
-            console.log("Mixer: " + mixers);
-
-            let cocktailObj = []
-
-            // [
-            //     {
-            //         alcohol: null,
-            //         cocktails: [
-            //             {
-            //                 name: null,
-            //                 price: null,
-            //                 image: null,
-            //             },
-            //         ]
-            //     }
-            // ]
-
-            alcohols.forEach(alc => {
-                cocktailObj.push(
-                    {
-                        alcohol: alc,
-                        cocktails: [],
-                    }
-                );
-            });
-
-            cocktailObj.forEach((cocObj, alcNum) => {
-                mixers.forEach((mix, mixNum) => {
-                    cocktails.forEach(coc => {
-                        if (coc.alcohols.includes(cocObj.alcohol) && coc.mixers.includes(mix)) {
-                            const curCocktail =
-                            {
-                                name: coc.name,
-                                price: 5.00,
-                                image: coc.image,
-                                alcohol: {
-                                    name: cocObj.alcohol,
-                                    container: alcNum,
-                                },
-                                mixer: {
-                                    name: mix,
-                                    container: mixNum,
-                                },
-                            };
-
-                            if (!cocObj.cocktails.includes(curCocktail))
-                                cocObj.cocktails.push(curCocktail);
-                        }
-                    });
-                });
-
-                // Sort by name
-                cocObj.cocktails.sort((a, b) => {
-                    return a.name.localeCompare(b.name);
-                });
-
-                // Remove duplicates
-                for (let i = 0; cocObj.cocktails[i] != null; ++i) {
-                    if (cocObj.cocktails[i + 1] != null &&
-                        cocObj.cocktails[i].name == cocObj.cocktails[i + 1].name) {
-                        cocObj.cocktails.splice(i + 1, 1);
-                        --i;
-                    }
-                }
-            });
-
-            console.log(cocktailObj);
-
-            res.send(cocktailObj);
-
+    db.any(`SELECT machine_cocktails
+            FROM machines
+            WHERE machine_id=${machine};`)
+        .then((cocktail) => {
+            cocktail = cocktail[0].machine_cocktails;
+            cocktail = JSON.parse(cocktail);
+            res.send(cocktail)
         })
         .catch(err => {
             console.log(err);
+            res.send('Could not get cocktails');
+        });
+})
+
+app.post('/:machine', (req, res) => {
+    let machine = req.params.machine;
+    let cocktails = req.body.cocktails;
+
+    db.any(`UPDATE machines
+	        SET machine_cocktails='${cocktails}'
+	        WHERE machine_id=${machine};`)
+        .then(() => {
+            res.send('Successfully added cocktails')
+        })
+        .catch(err => {
+            console.log(err);
+            res.send('Could not add cocktails');
         });
 
-});
+})
 
 app.listen(5000);
