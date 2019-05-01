4c4497a09c277879a3652b0179748c31730dbe36
Dylan Wright <sealsurlaw@gmail.com>
2019-04-30 19:53:04 -0700

    Added update empty status

--- server/notify.js
+++ server/notify.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const bodyParser = require('body-parser');
 
 const db = require('../db');
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+// Ping phones
 app.post('/:machine', (req, res, next) => {
     const machine = req.params.machine;
 
-    const query = `SELECT token_push_token
+    const query = `
+    SELECT token_push_token
     FROM tokens
     LEFT JOIN machines
     ON machine_user_id= token_user_id
@@ -50,5 +58,68 @@ app.post('/:machine', (req, res, next) => {
                 })
         })
 });
+
+app.post('/', (req, res) => {
+    const machine = req.body.machine;
+    const token = req.body.token;
+    const container = req.body.container;
+
+    let query = `
+        SELECT machine_id
+        FROM machines
+        LEFT JOIN users
+        ON user_id=machine_user_id
+        WHERE user_token='${token}'
+            AND machine_id=${machine}
+    ;`;
+
+    // Check if token matches machine id
+    // by checking length of returned array
+    db.any(query)
+        .then(mach => {
+            if (mach.length == 0) {
+                res.sendStatus(404);
+                return
+            }
+
+            // Update machine empty status and time
+            // For correct container
+            query = `
+                UPDATE machines
+                SET machine_empty[${container + 1}]=true, machine_empty_time[${container + 1}]=NOW()
+                WHERE machine_id=${mach[0].machine_id}
+                RETURNING machine_id
+            ;`;
+            db.any(query)
+                .then(m => {
+
+                    // Ping phones
+                    axios.post(`/${m.machine_id}`)
+                        .then(ping => {
+                            res.send("Updated " + m.machine_id);
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.sendStatus(500);
+                        })
+
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.sendStatus(404);
+                })
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(401);
+        })
+
+    // Update db
+    // Join user token and machine
+    // Make sure token is associated with machine
+
+    // Call ping
+})
 
 app.listen(5005);
